e198258d58d81b0edeb4edc0d803d058e31876f5
Mykhailo Moroz <45667081+Fisher1456@users.noreply.github.com>
2022-06-21 16:08:16 -0400

    Update data.js

--- REST API/data.js
+++ REST API/data.js
@@ -1,32 +1,74 @@
 const data = [
-    {
-        "id": "1",
-        "name": "Titanic",
-        "director": "James Cameron",
-        "score": "87%",
-        "date": "Dec 19,1997"
-    },
-    {
-        "id": "2",
-        "name": "Star Wars: A New Hope",
-        "director": "George Lucas",
-        "score": "93%",
-        "date": "May 25, 1977"
-    },
-    {
-        "id": "3",
-        "name": "The Princess Bride",
-        "director": "Rob Reiner",
-        "score": "97%",
-        "date": "Oct 1, 1987"
-    },
-    {
-        "id": "4",
-        "name": "Morbius",
-        "director": "Daniel Espinosa",
-        "score": "16%",
-        "date": "Apr 1, 2022"
-    },
+  {
+    id: "1",
+    name: "Titanic",
+    director: "James Cameron",
+    score: "87%",
+    date: "Dec 19, 1997",
+  },
+  {
+    id: "2",
+    name: "Star Wars: A New Hope",
+    director: "George Lucas",
+    score: "93%",
+    date: "May 25, 1977",
+  },
+  {
+    id: "3",
+    name: "The Princess Bride",
+    director: "Rob Reiner",
+    score: "97%",
+    date: "Oct 1, 1987",
+  },
+  {
+    id: "4",
+    name: "Morbius",
+    director: "Daniel Espinosa",
+    score: "16%",
+    date: "Apr 1, 2022",
+  },
+  {
+    id: "5",
+    name: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    score: "91%",
+    date: "Sep 22, 1994",
+  },
+  {
+    id: "6",
+    name: "The Godfather",
+    director: "Francis Ford Coppola",
+    score: "97%",
+    date: "Mar 24, 1972",
+  },
+  {
+    id: "7",
+    name: "The Lord of the Rings: The Return of the King",
+    director: "Peter Jackson",
+    score: "91%",
+    date: "Dec 19, 2001",
+  },
+  {
+    id: "8",
+    name: "12 Angry Men",
+    director: "Sidney Lumet",
+    score: "100%",
+    date: "Apr 10, 1957",
+  },
+  {
+    id: "9",
+    name: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    score: "92%",
+    date: "Oct 14, 1994",
+  },
+  {
+    id: "10",
+    name: "The Good, the Bad and the Ugly",
+    director: "Sergio Leone",
+    score: "97%",
+    date: "Dec 23, 1966",
+  },
 ];
 
 export default data;
