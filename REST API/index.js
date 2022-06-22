bde6bd15e968647c483f229977ea0a11ae844003
Quinton Mak <qclockwork@gmail.com>
2022-06-22 16:56:17 -0400

    query id or name in /movies path

--- REST API/index.js
+++ REST API/index.js
@@ -15,6 +15,22 @@ app.use(bodyParser.json()).use(cors());
 
 import data from './data.js';
 app.get('/movies', (req, res) => {
+    const {id, name} = req.query;
+    if (id) {
+        const movie = data.find(movie => movie.id == id);
+        if (!movie) {
+            res.status(404).json({error: "id does not exist"});
+        }
+        res.json(movie);
+    } else if (name) {
+        console.log(name);
+        const movie = data.find(movie => movie.name == name);
+        if (!movie) {
+            res.status(404).json({error: "movie with this name does not exist"});
+        }
+        res.json(movie);
+    }
+    
     res.json(data);
 });
 
